b3f034f91936a2eed6f93367fcbbc938e2f692e7
Your Name <youremail@domain.com>
2021-03-15 11:03:31 +0700

    update

--- Themes/Storefront/views/public/layout.blade.php
+++ Themes/Storefront/views/public/layout.blade.php
@@ -3,6 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        @hasSection('title')
+            @yield('title') - {{ setting('store_name') }}
+        @else
+            {{ setting('store_name') }}
+        @endif
+    </title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @stack('meta')
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/owl-slider.css') }}"/>
